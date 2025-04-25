09d27b0a5d066ef9e3b121163e8915634b17eecb
Jemal-G <jgeda@madisoncollege.edu>
2025-04-24 21:23:14 -0500

    trying server vs client components

--- app/component/locationFinderServer.tsx
+++ app/component/locationFinderServer.tsx
@@ -6,6 +6,7 @@ export default async function LocationFinderClient() {
     const response = await fetch("https://ipapi.co/json");
     const locationData = await response.json();
     console.log(locationData);
+    console.log("wel come")
   const locationInfo = locationData;
 
 
